58827319d09962ef10ccadc2b9ee02b3cd53faae
Shiva <shivabhattarai15@gmail.com>
2025-11-12 11:12:41 +0545

    add curve to /test/complete and /test/analyze page

--- frontend/src/app/test/analyze/page.tsx
+++ frontend/src/app/test/analyze/page.tsx
@@ -16,6 +16,8 @@ export default function TestAnalyzePage() {
     return () => clearTimeout(timer);
   }, [router]);
   return (
+    <main className="w-full h-full">
+      <div className="-z-10 fixed w-[1200px] lg:w-[2381px] h-[1200px] lg:h-[2298px] rounded-full left-[-320px] lg:left-[-400px] top-[400px] lg:top-[500px] shadow-[0_4px_100px_0_#6300FF]"></div>
       <div className="w-full lg:w-fit text-center absolute top-1/2 left-1/2 -translate-1/2 p-0 lg:p-10">
         <Image
           width={80}
@@ -30,5 +32,6 @@ export default function TestAnalyzePage() {
         </h1>
         <h1 className="text-sm lg:text-base font-bold">Career Path....</h1>
       </div>
+    </main>
   );
 }
--- frontend/src/app/test/complete/page.tsx
+++ frontend/src/app/test/complete/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import done from "@/images/activities/done.gif";
 
 export default function TestCompletePage() {
   const router = useRouter();
@@ -15,9 +17,21 @@ export default function TestCompletePage() {
   }, [router]);
 
   return (
-    <main className="px-4 md:px-6 lg:px-14">
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-base font-bold">Test is Successfully Completed</h1>
+    <main className="w-full h-full">
+      <div className="-z-10 fixed w-[1200px] lg:w-[2381px] h-[1200px] lg:h-[2298px] rounded-full left-[-320px] lg:left-[-400px] top-[400px] lg:top-[500px] shadow-[0_4px_100px_0_#6300FF]"></div>
+      <div className="lg:w-fit text-center absolute top-1/2 left-1/2 -translate-1/2 p-0 lg:p-10">
+        <Image
+          width={80}
+          height={80}
+          src={done}
+          unoptimized
+          alt="Analyzing"
+          className="mx-auto"
+        />
+        <h1 className="text-xs lg:text-base font-bold">
+          Test is Successfully Completed
+        </h1>
+        <h1 className="text-sm lg:text-base font-bold">Career Path....</h1>
       </div>
     </main>
   );
